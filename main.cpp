8968ebba78e87e489c0e1d01231ce48ea78d153c
大和 宮﨑 <k023g0041@g.neec.ac.jp>
2024-04-12 13:09:05 +0900

    終わり

--- main.cpp
+++ main.cpp
@@ -1,7 +1,55 @@
 #include <Novice.h>
-
+#include <math.h>
 const char kWindowTitle[] = "LD2B_07_ミヤザキ_ヤマト";
 
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
+
+Vector3 Add(const Vector3& v1, const Vector3& v2) {
+	Vector3 v3;
+	v3.x = v1.x + v2.x;
+	v3.y = v1.y + v2.y;
+	v3.z = v1.z + v2.z;
+	return v3;
+}
+Vector3 Subtract(const Vector3& v1, const Vector3& v2) {
+	Vector3 v3;
+	v3.x = v1.x - v2.x;
+	v3.y = v1.y - v2.y;
+	v3.z = v1.z - v2.z;
+	return v3;
+}
+Vector3 Multiply(float scalar, const Vector3& v) {
+	Vector3 v2;
+	v2.x = v.x * scalar;
+	v2.y = v.y * scalar;
+	v2.z = v.z * scalar;
+	return v2;
+}
+float Dot(const Vector3& v1, const Vector3& v2) {
+	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+float Length(const Vector3& v) {
+	return sqrtf(Dot(v, v));
+}
+Vector3 Normalize(const Vector3& v) {
+	Vector3 v2;
+	v2.x = v.x / Length(v);
+	v2.y = v.y / Length(v);
+	v2.z = v.z / Length(v);
+	return v2;
+}
+static const int KColumnWidth = 60;
+static const int KColumnHight = 20;
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + KColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + KColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + KColumnWidth * 3, y, "%s", label);
+}
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -11,6 +59,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+	//
+	Vector3 v1{ 1.0f,3.0f,-5.0f };
+	Vector3 v2{ 4.0f,-1.0f,2.0f };
+	float k{ 4.0f };
+	//
+	Vector3 resultAdd = Add(v1, v2);
+	Vector3 resultSubtract = Subtract(v1, v2);
+	Vector3 resultMultiply = Multiply(k, v1);
+	float resultDot = Dot(v1,v2);
+	float resultLength = Length(v1);
+	Vector3 resultNormalize = Normalize(v2);
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -32,7 +91,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-
+		VectorScreenPrintf(0, 0, resultAdd, " : Add");
+		VectorScreenPrintf(0, KColumnHight, resultSubtract, " : Subtract");
+		VectorScreenPrintf(0, KColumnHight * 2, resultMultiply, " : Multiply");
+		Novice::ScreenPrintf(0, KColumnHight * 3, "%.02f : Dot", resultDot);
+		Novice::ScreenPrintf(0, KColumnHight * 4, "%.02f : Length", resultLength);
+		VectorScreenPrintf(0, KColumnHight * 5, resultNormalize, " : Normalize");
 		///
 		/// ↑描画処理ここまで
 		///
